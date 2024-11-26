9773b757088c7256b7caa6177707754567f9257d
mahmed0009 <2704muhammadahmed@gmail.com>
2024-11-26 22:10:56 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, sized_box_for_whitespace, library_private_types_in_public_api, non_constant_identifier_names
 
-import 'package:accadassist/simpleCalculatorLightTheme.dart';
+import 'package:accadassist/simpleCalculator.dart';
 import 'package:flutter/material.dart';
 
 void main() {
